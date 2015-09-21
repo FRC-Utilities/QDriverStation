f7f300805faecf031bedb0564a0dfdc9a8e3ade7
Alex Spataru <alex.racotta@gmail.com>
2015-09-20 23:57:19 -0500

    Remove submodules

--- pre-commit.bat
+++
@@ -1,9 +0,0 @@
-::
-:: Don't be a douchebag, please run this script before making a commit!
-::
-:: This script will make sure that we use the lastest versions of the registered
-:: submodules and will format the source code automatically
-
-cd etc/scripts
-sync.bat
-format-code.bat
--- pre-commit.sh
+++
@@ -1,9 +0,0 @@
-#
-# Don't be a douchebag, please run this script before making a commit!
-#
-# This script will make sure that we use the lastest versions of the registered
-# submodules and will format the source code automatically
-
-cd etc/scripts
-sh sync.sh
-sh format-code.sh
