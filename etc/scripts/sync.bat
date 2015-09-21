26cfeeb3b2d598768bfff0c1f44aea335740ceab
Alex Spataru <alex.racotta@gmail.com>
2015-09-20 22:58:54 -0500

    Update sync.bat

--- etc/scripts/sync.bat
+++ etc/scripts/sync.bat
@@ -1,7 +1,8 @@
-REM As you may have noticed, I implemented the DS library and the Falcon QML
-REM widgets as submodules. I still cannot say if that was a good idea...
-REM
-REM Run this script often to ensure that we use the lastest versions of the
-REM registered submodules.
+:: As you may have noticed, I implemented the DS library and the Falcon QML
+:: widgets as submodules. I still cannot say if that was a good idea...
+::
+:: Run this script often to ensure that we use the lastest versions of the
+:: registered submodules.
+::
 git submodule foreach git pull origin master
 
