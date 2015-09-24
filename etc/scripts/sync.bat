ca0a42364c92b10d6b033f32b23fb7d3fe538921
Alex Spataru <alex.racotta@gmail.com>
2015-09-23 20:32:41 -0500

    Update sync scripts

--- etc/scripts/sync.bat
+++ etc/scripts/sync.bat
@@ -5,8 +5,6 @@ git checkout master
 
 :: Let's push our changes back to the subtrees...
 git subtree push --prefix=lib/LibDS git@github.com:wint-3794/libds.git master
-git subtree push --prefix=lib/Falcon git@github.com:alex-spataru/falcon.git master
 
 :: Let's download the changes in the subtrees...
 git subtree pull --prefix=lib/LibDS git@github.com:wint-3794/libds.git master
-git subtree pull --prefix=lib/Falcon git@github.com:alex-spataru/falcon.git master
