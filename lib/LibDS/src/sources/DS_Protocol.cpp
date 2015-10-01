5363e84f2c110b2e22c92a5b6cfa45161a95caf4
Alex Spataru <alex.racotta@gmail.com>
2015-10-01 18:05:17 -0500

    Ensure robot is disabled if there is no communication

--- lib/LibDS/src/sources/DS_Protocol.cpp
+++ lib/LibDS/src/sources/DS_Protocol.cpp
@@ -67,7 +67,7 @@ void DS_Protocol::setAlliance (DS_Alliance alliance) {
 }
 
 void DS_Protocol::setControlMode (DS_ControlMode mode) {
-    p_controlMode = mode;
+    p_controlMode = p_robotCommunication ? mode : DS_ControlDisabled;
     emit controlModeChanged (mode);
 }
 
