2faeefb6ee090629c2ca787bc7bac34d6a6d8c21
Alex Spataru <alex_spataru@outlook.com>
2015-12-18 15:06:12 -0600

    Change directory structure of LibDS

--- etc/scripts/format-code.sh
+++ etc/scripts/format-code.sh
@@ -1,8 +1,19 @@
-# Style and format the source code recursively
-astyle --pad-oper --pad-first-paren-out --align-pointer=type --remove-brackets --convert-tabs --max-code-length=80 --style=google --lineend=windows --suffix=none --recursive ../../*.h ../../*.cpp
+s2#!/bin/bash
 
-# Notify the user that we have finished
+# Description: This script changes the style format of
+#              all the source code of the project.
+
+# Run only on the directory of the script
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+# Style and format recursively
+astyle --pad-oper --pad-first-paren-out --align-pointer=type --remove-brackets --convert-tabs --max-code-length=80 --style=google --lineend=windows --suffix=none --recursive ../*.h ../*.cpp
+
+# Notify the user that we are done
 echo
-echo Code styling complete!
+echo "Code styling complete!"
 echo
 
+# Let the user see the output
+read -r -p "Press any key to continue..." key
+clear
