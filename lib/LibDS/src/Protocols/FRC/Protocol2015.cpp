f1429420d1a5a4151b4546e1724e8258dda1a39f
Alex Spataru <alex_spataru@outlook.com>
2016-03-03 19:57:27 -0600

    Update Protocol2015.cpp

--- lib/LibDS/src/Protocols/FRC/Protocol2015.cpp
+++ lib/LibDS/src/Protocols/FRC/Protocol2015.cpp
@@ -38,50 +38,50 @@ const QString LIB_PATH = "/tmp/frc_versions/FRC_Lib_Version.ini";
 
 enum ProtocolStandards {
     /* Client -> RoboRIO operation states */
-    pEmStopOn                 = 0x80, /**< Emergency stop enabled */
-    pEmStopOff                = 0x00, /**< Emergency stop disabled */
-    pEnabled                  = 0x04, /**< Robot Enabled */
-    pDisabled                 = 0x00, /**< Robot Disabled */
-    pFMSAttached              = 0x01, /**< DS is attached to the FMS */
+    pEmStopOn                 = 0x80,
+    pEmStopOff                = 0x00,
+    pEnabled                  = 0x04,
+    pDisabled                 = 0x00,
+    pFMSAttached              = 0x01,
 
     /* Client -> RoboRIO operation modes */
-    pControlTest              = 0x01, /**< Individual actuators can be moved */
-    pControlAutonomous        = 0x02, /**< Robot takes over the world */
-    pControlTeleoperated      = 0x00, /**< User moves the robot */
+    pControlTest              = 0x01,
+    pControlAutonomous        = 0x02,
+    pControlTeleoperated      = 0x00,
 
     /* Client -> RoboRIO section codes */
-    pHeaderTime               = 0x0f, /**< Indicates start of current time data */
-    pHeaderGeneral            = 0x01, /**< Indicates a client packet */
-    pHeaderJoystick           = 0x0c, /**< Indicates start of JS section */
-    pHeaderTimezone           = 0x10, /**< Indicates start of Timezone data */
+    pHeaderTime               = 0x0f,
+    pHeaderGeneral            = 0x01,
+    pHeaderJoystick           = 0x0c,
+    pHeaderTimezone           = 0x10,
 
     /* Client -> RoboRIO special instructions */
-    pInstructionNormal        = 0x10, /**< Normal operation */
-    pInstructionInvalid       = 0x00, /**< No communication */
-    pInstructionRebootRIO     = 0x14, /**< Reboot the roboRIO */
-    pInstructionRestartCode   = 0x16, /**< Kill the user code */
+    pInstructionNormal        = 0x10,
+    pInstructionInvalid       = 0x00,
+    pInstructionRebootRIO     = 0x14,
+    pInstructionRestartCode   = 0x16,
 
     /* Client -> RoboRIO alliance & postion */
-    pRed1                     = 0x00, /**< Red alliance, position 1  */
-    pRed2                     = 0x01, /**< Red alliance, position 2  */
-    pRed3                     = 0x02, /**< Red alliance, position 3  */
-    pBlue1                    = 0x03, /**< Blue alliance, position 1 */
-    pBlue2                    = 0x04, /**< Blue alliance, position 2 */
-    pBlue3                    = 0x05, /**< Blue alliance, position 3 */
+    pRed1                     = 0x00,
+    pRed2                     = 0x01,
+    pRed3                     = 0x02,
+    pBlue1                    = 0x03,
+    pBlue2                    = 0x04,
+    pBlue3                    = 0x05,
 
     /* RoboRIO -> Client extra data */
-    pRIOHeaderJoystickOutput  = 0x01, /**< Asks us to rumble joysticks */
-    pRIOHeaderDiskInformation = 0x04, /**< Gives us roboRIO disk metrics */
-    pRIOHeaderCPUInformation  = 0x05, /**< Gives us roboRIO CPU metrics */
-    pRIOHeaderRAMInformation  = 0x06, /**< Gives us roboRIO RAM metrics */
-    pRIOHeaderCANMetrics      = 0x0e, /**< Gives us robot CAN net metrics */
+    pRIOHeaderJoystickOutput  = 0x01,
+    pRIOHeaderDiskInformation = 0x04,
+    pRIOHeaderCPUInformation  = 0x05,
+    pRIOHeaderRAMInformation  = 0x06,
+    pRIOHeaderCANMetrics      = 0x0e,
 
     /* RoboRIO -> Client echo codes */
-    pProgramTest              = 0x01, /**< Control mode confirmation code */
-    pProgramAutonomous        = 0x02, /**< Control mode confirmation code */
-    pProgramCodePresent       = 0x20, /**< Not very elegant, but it works */
-    pProgramTeleoperated      = 0x00, /**< Control mode confirmation code */
-    pProgramRequestTime       = 0x01, /**< Robot wants to get the date/time */
+    pProgramTest              = 0x01,
+    pProgramAutonomous        = 0x02,
+    pProgramCodePresent       = 0x20,
+    pProgramTeleoperated      = 0x00,
+    pProgramRequestTime       = 0x01,
 };
 
 //=============================================================================
@@ -255,12 +255,14 @@ bool FRC_Protocol2015::_readRobotPacket (QByteArray data) {
         int id = extended.at (1);
 
         /* Robot wants us to rumble a joystick */
-        if (id == pRIOHeaderJoystickOutput)
-            qDebug() << Q_FUNC_INFO << "TODO: Joystick Output";
+        if (id == pRIOHeaderJoystickOutput) {
+            // TODO
+        }
 
         /* Packet contains roboRIO CPU info */
-        else if (id == pRIOHeaderCPUInformation)
-            qDebug() << Q_FUNC_INFO << "TODO: CPU information";
+        else if (id == pRIOHeaderCPUInformation) {
+            // TODO
+        }
 
         /* Packet contains roboRIO RAM info */
         else if (id == pRIOHeaderRAMInformation) {
@@ -277,8 +279,9 @@ bool FRC_Protocol2015::_readRobotPacket (QByteArray data) {
         }
 
         /* Packet contains CAN metrics data */
-        else if (id == pRIOHeaderCANMetrics)
-            qDebug() << Q_FUNC_INFO << "TODO: CPU information" << extended.toHex();
+        else if (id == pRIOHeaderCANMetrics) {
+            // TODO
+        }
     }
 
     /* Packet was successfully read, reset watchdog */
@@ -316,11 +319,11 @@ QByteArray FRC_Protocol2015::_getClientPacket() {
 
     /* Construct the packet */
     data.append (DS::intToBytes (sentRobotPackets()));
-    data.append (pHeaderGeneral);                  // Protocol version code
-    data.append (opcode);                          // Operation code
-    data.append (instruction);                     // Special instructions
-    data.append (getAllianceCode());               // Alliance & position
-    data.append (extensions);                      // Joystick data or UTC info
+    data.append (pHeaderGeneral);
+    data.append (opcode);
+    data.append (instruction);
+    data.append (getAllianceCode());
+    data.append (extensions);
 
     return data;
 }
